b7f4817fefe9536531c09c76920914cb988292e6
T-PCnew <qwertzarrow25bs@gmail.com>
2025-12-15 22:34:45 +0100

    migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ReservationEntity> Reservations { get; set; }
+        private string DbPath { get; set; }
+
+        public AppDbContext()
+        {
+            DbPath = @"E:\data\reservations.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ReservationEntity>().HasData(
+                new ReservationEntity() 
+                { 
+                    Id = 1, 
+                    City = "Kraków", 
+                    Address = "Wysłouchów 22", 
+                    RoomName = "44",
+                    Owner = "Jan Kowalski",
+                    Price = 500m,
+                    Date = new DateTime(2025, 05, 20)
+                },
+                new ReservationEntity() 
+                { 
+                    Id = 2, 
+                    City = "Miechów", 
+                    Address = "Polna 44", 
+                    RoomName = "546",
+                    Owner = "Anna Nowak",
+                    Price = 123123m,
+                    Date = new DateTime(2025, 06, 15)
+                }
+            );
+        }
+    }
+}
---
+++ Data/Entities/ReservationEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities
+{
+    [Table("reservations")]
+    public class ReservationEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string RoomName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Owner { get; set; }
+
+        [Column("price")] 
+        public decimal? Price { get; set; }
+
+        [Column("reservation_date")]
+        public DateTime? Date { get; set; }
+    }
+}
