99a0ebcb1650527f97fef039173f3a00795377d3
T-PCnew <qwertzarrow25bs@gmail.com>
2025-12-15 20:11:28 +0100

    reservation

---
+++ WebApplication1/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Reservation model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Result", model);
+            }
+            return View("Index", model);
+        }
+    }
+}
